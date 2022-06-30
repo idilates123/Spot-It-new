d287a05aa4e0f6e9f65cf4421a35a2e8cff5bdc3
idil <idila2023@stu.koc.k12.tr>
2022-06-30 22:42:08 +0300

    Initial Commit

---
+++ Spot-It/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Spot-It
+//
+//  Created by Jasmine Ma on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    
+    @IBOutlet weak var TitleImage: UIImageView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
